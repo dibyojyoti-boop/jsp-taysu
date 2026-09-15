0b959b5e052d33a0cfd23c2b8ba4f1b2b714998e
dibyojyoti-boop <youtubechanale66@gmail.com>
2026-09-15 13:30:29 +0530

    Add initial Capacitor configuration file

---
+++ android-app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.jsptaysu.taysu',
+  appName: 'TAYSU',
+  webDir: 'www'
+};
+
+export default config;
